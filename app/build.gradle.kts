dcb2641a1ed81a9f530ed4b879ff868b4c541d32
Ronald Treier <ronbo4512@icloud.com>
2025-10-23 16:40:03 -0400

    q

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.ai)
     implementation(libs.firebase.storage)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/artmarketplace/CustomerHomeActivity.java
+++ app/src/main/java/com/example/artmarketplace/CustomerHomeActivity.java
@@ -1,19 +1,50 @@
 package com.example.artmarketplace;
 
 
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
+import com.google.firebase.firestore.*;
 
 
 
 public class CustomerHomeActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+    private DatabaseReference mDatabase;
+    private TextView artList;
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
-
-
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        artList = findViewById(R.id.artList);
+        String artId = auth.getUid();
+        mDatabase.child("art").child(artId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                    artList.setText(task.getException().toString());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    artList.setText(task.getResult().getValue().toString());
+                }
+            }
+        });
     }
 }
